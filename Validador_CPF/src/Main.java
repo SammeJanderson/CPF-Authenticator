07a177ac5a874977a539a29d5e14c18e9c633492
Samme Janderson <samme.san@gmail.com>
2020-10-22 16:52:53 -0300

    bug fixed on CPF generator

--- Validador_CPF/src/Main.java
+++ Validador_CPF/src/Main.java
@@ -18,13 +18,15 @@ public class Main {
             case "1":
                 System.out.println("GERAR QUANTOS CPFs?");
                 option = scanner.next();
+
                 for (int i = 0; i < Integer.parseInt(option); i++) {
                     fakeCPF.makeFakeCPF();
                 }
                 break;
             case "2":
                 System.out.println("DIGITE OS 11 NUMEROS DO CPF");
-                verficadorCPF.verificador(scanner.next());
+                String op = scanner.next();
+                verficadorCPF.verificador(op);
                 break;
             default:
                 System.out.println("Opção invalida");
